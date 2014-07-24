13be0fa269b49b131a3576f717d7b14ca7abfa85
Alex Conway <alexconway808@gmail.com>
2014-07-23 18:42:52 -1000

    setup mongo

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 var mongoose = require ('mongoose');
 var jade = require ('jade');
 var express = require ('express');
-var bodyparser = require ('body-parser');
-var methodoverride = require ('method-override')
+var app = express();
+var bodyParser = require ('body-parser');
+var methodoverride = require ('method-override');
 
-mongoose.connect = 
+mongoose.connect('mongodb://user1:1234@ds037607.mongolab.com:37607/teuxdeuxpartdeux');
 
 //This is from the express api documentation
-app.use(express.static(_dirname + ‘/public’));
+app.use(express.static(__dirname + '/public'));
 
 //Create the Task schema
 var Schema = mongoose.Schema;
@@ -21,6 +22,12 @@ var taskSchema = new Schema({
 //Define the Task model
 var Task = mongoose.model('Task', taskSchema);
 
+app.set('views', __dirname + '/Templates');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 //Is express running?
 
 //Is jade running?
@@ -31,47 +38,50 @@ var Task = mongoose.model('Task', taskSchema);
 // INDEX
 
 // GET /tasks - LIST works with Jade templates
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
-
-
-//Post /tasks - CREATE a redirect
-app.post("/tasks", function (req, res) {
-  // console.log("Receive task post?")
-  var task = new TaskItem ({
-    title: req.param (‘title’),   // This is in the express api req param name
-    notes: req.param (‘notes’) 
-
-  task.save(function (wert, task) {
-    if(wert){res.send(500, wert);}
-
-    res.redirect("/");
-  
+app.get('/tasks', function (req, res) {
+  Task.find(function (err, tasks){
+    console.log(tasks);
+    res.render('Tasks/List.jade',{tasks: tasks});
   });
-
 });
 
 
-//GET /tasks/:id - SHOW with Jade template
+// //Post /tasks - CREATE a redirect
+// app.post("/tasks", function (req, res) {
+//   // console.log("Receive task post?")
+//   var task = new TaskItem ({
+//     title: req.param ('title'),   // This is in the express api req param name
+//     notes: req.param ('notes') 
 
-//GET /tasks/:id/edit - EDIT with a form
+//   task.save(function (wert, task) {
+//     if(wert){res.send(500, wert);}
 
-//PUT /tasks/:id - UPDATE with a redirect
-app.put ("/tasks/:id", function (req, res) {
-  var id = req.param(‘id’);  NOTE: This is just an example don’t just copy, find the ID in Mongolab and copy and paste it in
+//     res.redirect("/");
+  
+//   });
 
-  TaskItem.findOne({_id: id}, fucntion () {
-
-
-
-//DEL /tasks/:id - DESTROY with a redirect
-
-//GET /tasks/new - NEW form
+// });
 
 
-server.listen(1337, function () {
-  console.log('%s listening at %s', server.name, server.url);
+// //GET /tasks/:id - SHOW with Jade template
+
+// //GET /tasks/:id/edit - EDIT with a form
+
+// //PUT /tasks/:id - UPDATE with a redirect, when there is a post it saves a PUT on UPDATE
+// app.put ("/tasks/:id", function (req, res) {
+//   var id = req.param(‘id’);  NOTE: This is just an example don’t just copy, find the ID in Mongolab and copy and paste it in
+
+//   TaskItem.findOne({_id: id}, fucntion () {
+
+
+
+// //DEL /tasks/:id - DESTROY with a redirect
+
+// //GET /tasks/new - NEW form
+
+
+app.listen(1337, function () {
+  console.log('%s listening at %s', app.name, app.url);
 });
 
 
