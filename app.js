6facf026f081f4376ab2108635911798efa8301c
Alex Conway <alexconway808@gmail.com>
2014-07-23 16:47:06 -1000

    setup the initial GET PUT tasks

--- app.js
+++ app.js
@@ -7,33 +7,8 @@ var methodoverride = require ('method-override')
 
 mongoose.connect = 
 
-
-// CRUD task items
-
-// INDEX
-
-// GET /
-
-// GET 
-// GET /tasks
-
-// SHOW
-
-// GET /tasks/:id
-
-// NEW
-
-// GET /tasks/new
-
-// CREATE
-// Post /tasks
-
-// EDIT
-// GET /tasks/
-
-// UPDATE
-// DELETE
-
+//This is from the express api documentation
+app.use(express.static(_dirname + ‘/public’));
 
 //Create the Task schema
 var Schema = mongoose.Schema;
@@ -46,63 +21,69 @@ var taskSchema = new Schema({
 //Define the Task model
 var Task = mongoose.model('Task', taskSchema);
 
+//Is express running?
 
-var server = restify.createServer({
-  name: 'app',
-  version: '0.0.0'
-
-});
-
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
+//Is jade running?
 
 
-server.get("/", function (req, res) {
+// CRUD task items
 
-   //Display the Tasks in the browser at localhost: 1337
-  // Task.find(function (err, Tasks){
-  //   for (i in Tasks){
-  //     body += Tasks[i].task_data + '<br>';
-  //   }
-  //   res.writeHead(200, {
-  //     'Content-Length': Buffer.byteLength(body),
-  //     'Content-Type' : 'text/html'
-  //   });
+// INDEX
 
-  //   res.write(body);
-  //   res.send();
-  // });
-  var options = {};
-  var html = jade.renderFile('./templates/index.jade', options);
-
-  res.writeHead(200, {
-    'Content-Length': Buffer.byteLength(html),
-    'Content-Type': 'text/html'
-  });
-  
-  res.write(html);
-  res.end();
-
+// GET /tasks - LIST works with Jade templates
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
 
 
-server.post('/task', function (req, res){
+//Post /tasks - CREATE a redirect
+app.post("/tasks", function (req, res) {
   // console.log("Receive task post?")
-  var task = new Task ({
-    task_data: req.body.task_data
+  var task = new TaskItem ({
+    title: req.param (‘title’),   ← This is in the express api req param name
+    notes: req.param (‘notes’) 
+
+  task.save(function (wert, task) {
+    if(wert){res.send(500, wert);}
+
+    res.redirect("/");
+  
   });
-  // console.log(task);
-  task.save(function (err){
-    // console.log(err);
-    if (err) res.send(err);
-
-    res.send("Your Tasks have been saved");
-});
 
 });
+
+
+//GET /tasks/:id - SHOW with Jade template
+
+//GET /tasks/:id/edit - EDIT with a form
+
+//PUT /tasks/:id - UPDATE with a redirect
+
+//DEL /tasks/:id - DESTROY with a redirect
+
+//GET /tasks/new - NEW form
 
 
 server.listen(1337, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
+
+
+
+
+
+
+
+
+
+//Need this?
+// var server = restify.createServer({
+//   name: 'app',
+//   version: '0.0.0'
+
+// });
+
+// server.use(restify.acceptParser(server.acceptable));
+// server.use(restify.queryParser());
+// server.use(restify.bodyParser());
+
