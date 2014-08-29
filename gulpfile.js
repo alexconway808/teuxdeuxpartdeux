ef8daec6aaf4c193427533cbc31bbcbf1dc6b4b1
Alex Conway <alexconway808@gmail.com>
2014-08-28 21:50:10 -1000

    moved vertical lines and added horizontal lines

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,58 @@
-var gulp = require('gulp');
-var minify = require('gulp-minify-css');
-var less = require('gulp-less');
+//gulp modules
+var gulp = require('gulp'),
+    minify = require('gulp-minify-css'),
+    less = require('gulp-less'),
+    uglify = require('gulp-uglify'),//
+    livereload = require('gulp-livereload'),
+    nodemon = require('gulp-nodemon'),//
+    plumber = require('gulp-plumber'),//
+    notify = require('gulp-notify');//
+
+function onError(err) {
+  notify.onError(err.message)(err);
+  this.emit('end');
+}
+
+//path for tasks
+var paths = {
+  less: './assets/less/*.less',
+  js: './assets/javascripts/*.js'
+}
 
 gulp.task('less', function() {
-  return gulp.src('./assets/less/styles.less')
+  return gulp.src(paths.less)
+    .pipe(plumber(onError))
     .pipe(less())
     .pipe(gulp.dest('./public/stylesheets/'));
-
 });
 
+gulp.task('js', function () {
+  return gulp.src(paths.js)
+  pipe(uglify())
+  pipe(gulp.dest('./assets/javascripts'))
+})
 
-gulp.task('default', ['css']);
+gulp.task('serve', function() {
+  nodemon({
+    script: 'app.js',
+    ext: 'js',
+    ignore: ['/assets/**', '/public/**']
+  }).on('restart', function () {
+    console.log('restarted! ' + (new Date()));
+  });
+})
 
-//watch these files for css files and when they are changed then 
-//trigger a bunch of tasks
-gulp.task('watch', function() {
-  gulp.watch('less/style.less', ['less']);
+gulp.task('watch', function () {
 
-//this will watch any compiled file when changed it will take in the 
-//file and run the refresh change file command
-  gulp.watch('.public/**/*')
+  livereload.listen();
+
+  gulp.watch(paths.less, ['less']);
+  gulp.watch(paths.js, ['js']);
+  gulp.watch(['public/**/*', 'templates/**/*.jade'])
     .on('change', function(file) {
       livereload.changed(file.path);
     });
-
-
 });
+
+gulp.task('build', ['less', 'js']);
+gulp.task('default', ['build', 'serve', 'watch']);
